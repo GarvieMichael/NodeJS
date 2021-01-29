3ceb0ea84b3df8b6e18d76a28d20c566a04abbff
GarvieMichael <mmgarvie@gmail.com>
2021-01-29 20:09:51 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var user = process.env.DB_USER;
 var password =process.env.DB_USER_PW;
 
-var mongo_route = 'mongodb://' + user + ':' + password + '@' + fc-database + '/' + fc-final-test-node
+var mongo_route = 'mongodb://' + user + ':' + password + '@fc-database/fc-final-test-node'
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', number: Math.random(), secret_boi: process.env.DB_NAME, mongo_route: mongo_route });
