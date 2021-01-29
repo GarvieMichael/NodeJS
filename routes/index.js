4c285c4213dfd79408ebaff1627e8b0d70a3ebfe
GarvieMichael <mmgarvie@gmail.com>
2021-01-29 12:04:07 +0200

    Checking DB Variables

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', number: Math.random(), secret_boi: process.env.TEST });
+  res.render('index', { title: 'Express', number: Math.random(), secret_boi: process.env.DB_NAME });
 });
 
 module.exports = router;
