83b2214bad1750dff660b8b611d41a1ceb469d09
GarvieMichael <mmgarvie@gmail.com>
2021-01-31 16:37:32 +0200

    Logging the uris

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var admin = 'admin'
 var admin_password = process.env.DB_ADMIN_PW;
 const dbName = process.env.DB_NAME;
 
-// const mongo_route = 'mongodb://' + user + ':' + password + '@fc-database/fc-final-test-node'
-// const mongo_route = 'mongodb://' + admin + ':' + admin_password + '@fc-database/fc-final-test-node'
+// const mongo_route_user = 'mongodb://' + user + ':' + password + '@fc-database/fc-final-test-node'
+// const mongo_route_admin = 'mongodb://' + admin + ':' + admin_password + '@fc-database/fc-final-test-node'
 const mongo_route = process.env.OPENSHIFT_MONGODB_DB_URL;
 
 
@@ -24,12 +24,13 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 MongoClient.connect(mongo_route, function(err, client) {
-  console.log(mongo_route)
+  console.log(mongo_route_user);
+  console.log(mongo_route_admin);
   assert.strictEqual(null, err);
   console.log("Connected successfully to server");
  
   const db = client.db(dbName);
- 
+  console.log(db)
   client.close();
 });
 
