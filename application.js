c9afe03e1826243b79e25376b6316921b5fe7ea6
GarvieMichael <mmgarvie@gmail.com>
2021-02-08 08:50:39 +0200

    Update application.js

--- application.js
+++ application.js
@@ -9,7 +9,20 @@ application.use(logger('combined', { stream: require("./logs/log_helper")}))
 application.use("/", require("./config/routes"))
 
 // === Database Connectivity Testing ===
-database_helper.test_connectivity()
+const user = process.env.DB_USER;
+const user_password = process.env.DB_USER_PW;
+const dbName = process.env.DB_NAME;
+const mongo_route = 'mongodb://' + user + ':' + user_password + '@' + dbName + '/' + dbName;
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+MongoClient.connect(mongo_route, function(err, client) {
+  assert.strictEqual(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  client.close();
+});
 
 //  === Engine Setup ===
 application.listen(8080);
